9cdef5bd3c4616d3bc83cc672eaef052844a8282
MrAntiTesla <81398108+MrAntiTesla@users.noreply.github.com>
2021-06-15 18:47:08 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ console.log("x="+last_position_of_x+"y=" +last_position_of_y);
 ctx.moveTo(last_position_of_x, last_position_of_y);
 console.log("Current position of x and y coordinates =");
 console.log("x="+current_position_of_mouse_x+"y=" +current_position_of_mouse_y);
-ctx.moveTo(current_position_of_mouse_x, current_position_of_mouse_y);
+ctx.lineTo(current_position_of_mouse_x, current_position_of_mouse_y);
 ctx.stroke();    
 }    
 last_position_of_x=current_position_of_mouse_x;
@@ -44,4 +44,40 @@ mouseEvent="mouseleave";
 function Cleararea()
 {
 ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-}    
+}
+var width=screen.width;
+new_width=screen.width-70;
+new_height=screen.height-300;
+if(width<992)
+{
+document.getElementById("myCanvas").width=new_width;
+document.getElementById("myCanvas").height=new_height;
+document.body.style.overflow="hidden";  
+}
+canvas.addEventListener("touchstart", my_touchstart);
+function  my_touchstart(e)
+{
+console.log("my_touchstart");
+last_position_of_x=e.touches[0].clientX-canvas.offsetLeft;
+last_position_of_y=e.touches[0].clienty-canvas.offsetTop;  
+}
+canvas.addEventListener("touchmove", my_touchmove);
+function my_touchmove(e)
+{
+console.log("my_touchMove");
+current_position_of_touch_x=e.touches[0].clientX-canvas.offsetLeft;
+current_position_of_touch_y=e.touches[0].clienty-canvas.offsetTop;  
+}
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=width_of_line;
+console.log("Last position of x and y coordinates=");
+console.log("x="+last_position_of_x+"y="+last_position_of_y);
+ctx.moveTo(last_position_of_x, last_position_of_y);
+console.log("Current position of x and y coordinates=");
+console.log("x="+current_position_of_touch_x+"y="+current_position_of_touch_y)
+ctx.lineTo(current_position_of_touch_x, current_position_of_touch_y);
+ctx.stroke();
+last_position_of_x=current_position_of_touch_x
+last_position_of_y=current_position_of_touch_y
+  
